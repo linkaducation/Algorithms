3e277fd4f801a519ecc98e3799dc1b7a56f2e80a
lovepandababy <iterellen@163.com>
2017-05-06 13:49:18 +0800

    5.6

---
+++ src/org/Algorithm/QueueWithStack.java
@@ -0,0 +1,53 @@
+package org.Algorithm;
+
+import java.util.Stack;
+
+/**
+ * Created by Ellen on 2017/5/6.
+ * 用两个栈来实现一个队列，完成队列的Push和Pop操作
+ * 队列中的元素为int类型。
+ */
+public class QueueWithStack {
+//    Stack<Integer> stack1 = new Stack<Integer>();
+//    Stack<Integer> stack2 = new Stack<Integer>();
+//    boolean flag = true;
+//
+//    public void push(int node) {
+//        if (!flag) {
+//            while (!stack2.isEmpty()) {
+//                stack1.push(stack2.pop());
+//            }
+//        }
+//        stack1.push(node);
+//        flag = true;
+//    }
+//
+//    public int pop() {
+//        if (flag) {
+//            while (!stack1.isEmpty()) {
+//                stack2.push(stack1.pop());
+//            }
+//            flag = false;
+//            return stack2.pop();
+//        }
+//        return stack2.pop();
+//    }
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if(stack1.empty()&&stack2.empty()){
+            throw new RuntimeException("Queue is empty!");
+        }
+        if(stack2.empty()){
+            while(!stack1.empty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+}
