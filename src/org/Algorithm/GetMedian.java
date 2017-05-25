a2acb6914b7844d928b1831ab3904e5c9c43a697
lovepandababy <iterellen@163.com>
2017-05-25 20:57:19 +0800

    5.25

---
+++ src/org/Algorithm/GetMedian.java
@@ -0,0 +1,19 @@
+package org.Algorithm;
+
+/**
+ * Created by Ellen on 2017/5/25.
+ * 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值
+ */
+public class GetMedian {
+	public class Solution {
+
+		public void Insert(Integer num) {
+
+		}
+
+		public Double GetMedian() {
+
+		}
+
+	}
+}
--- src/org/Algorithm/isSymmetrical.java
+++ src/org/Algorithm/isSymmetrical.java
@@ -3,28 +3,24 @@ package org.Algorithm;
 import org.Algorithm.HelperClass.TreeNode;
 
 /**
- * Created by Ellen on 2017/5/18.
- * 请实现一个函数，用来判断一颗二叉树是不是对称的。
- * 注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
+ * Created by Ellen on 2017/5/18. 请实现一个函数，用来判断一颗二叉树是不是对称的。 注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
  */
 public class isSymmetrical {
-    boolean isSymmetrical(TreeNode pRoot) {
-        if (pRoot == null) {
-            return false;
-        }
-        return isSymmetricals(pRoot.left, pRoot.right);
-    }
+	boolean isSymmetrical(TreeNode pRoot) {
+		if (pRoot == null) {
+			return true;
+		}
+		return isSymmetricals(pRoot.left, pRoot.right);
+	}
 
-    private boolean isSymmetricals(TreeNode lNode, TreeNode rNode) {
-        if (lNode == null && rNode == null) {
-            return true;
-        }
-        if ((lNode != null && rNode == null) || (lNode == null && rNode != null)) {
-            return false;
-        }
-        if (lNode != rNode) {
-            return false;
-        }
-        return isSymmetricals(lNode.left, rNode.left) && isSymmetricals(lNode.right, rNode.right);
-    }
+	private boolean isSymmetricals(TreeNode lNode, TreeNode rNode) {
+		if (lNode == null) {
+			return rNode == null;
+		}
+		if (rNode == null) {
+			return false;
+		}
+		return lNode.val == rNode.val && isSymmetricals(lNode.left, rNode.right)
+				&& isSymmetricals(lNode.right, rNode.left);
+	}
 }
