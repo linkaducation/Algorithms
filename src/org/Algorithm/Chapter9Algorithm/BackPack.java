3fafefcd39d90e26cfab865e107c3ecbc691411a
lovepandababy <iterellen@163.com>
2017-06-03 21:51:36 +0800

    6.3

--- src/org/Algorithm/Chapter9Algorithm/BackPack.java
+++ src/org/Algorithm/Chapter9Algorithm/BackPack.java
@@ -5,17 +5,37 @@ package org.Algorithm.Chapter9Algorithm;
  * 3, 5]装入背包，最多可以装满10的空间。 如果背包的大小为12，可以选择[2, 3, 7]装入背包，最多可以装满12的空间。 函数需要返回最多能装满的空间大小。
  */
 public class BackPack {
-	public int backPack(int m, int[] A) {
-		// write your code here
-        if (m <= 0 || A.length <= 0) {
-            return 0;
-        }
-        int len = A.length;
-        int[] result = new int[len];//存放扫描结果
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < ; j++) {
-
+    public int backPack(int m, int[] A) {
+        boolean f[][] = new boolean[A.length + 1][m + 1];
+        for (int i = 0; i <= A.length; i++) {
+            for (int j = 0; j <= m; j++) {
+                f[i][j] = false;
             }
         }
+        f[0][0] = true;
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 0; j <= m; j++) {
+                f[i][j] = f[i - 1][j];
+                if (j >= A[i-1] && f[i-1][j - A[i-1]]) {
+                    f[i][j] = true;
+                }
+            } // for j
+        } // for i
+        for (int i = 0; i <= A.length; i++) {
+            for (int j = 0; j <= m; j++) {
+                if(f[i][j])
+                    System.out.print(1);
+                else
+                    System.out.print(0);
+            } // for j
+        }
+
+        for (int i = m; i >= 0; i--) {
+            if (f[A.length][i]) {
+                return i;
+            }
+        }
+
+        return 0;
     }
 }
