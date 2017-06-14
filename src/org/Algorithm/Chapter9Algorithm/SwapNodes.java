431c89701f62a81003a51a5979505d1ecd935abc
lovepandababy <iterellen@163.com>
2017-06-14 20:52:38 +0800

    6.14

--- src/org/Algorithm/Chapter9Algorithm/SwapNodes.java
+++ src/org/Algorithm/Chapter9Algorithm/SwapNodes.java
@@ -11,6 +11,45 @@ import org.Algorithm.SwordForOffer.HelperClass.ListNode;
 public class SwapNodes {
 	public ListNode swapNodes(ListNode head, int v1, int v2) {
 		// Write your code here
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
 
+        ListNode node1Prev = null, node2Prev = null;
+        ListNode cur = dummy;
+        while (cur.next != null) {
+            if (cur.next.val == v1) {
+                node1Prev = cur;
+            } else if (cur.next.val == v2) {
+                node2Prev = cur;
+            }
+            cur = cur.next;
+        }
+
+        if (node1Prev == null || node2Prev == null) {
+            return head;
+        }
+
+        if (node2Prev.next == node1Prev) {
+            // make sure node2Prev.next is not node1Prev
+            ListNode t = node1Prev;
+            node1Prev = node2Prev;
+            node2Prev = t;
+        }
+
+        ListNode node1 = node1Prev.next;
+        ListNode node2 = node2Prev.next;
+        ListNode node2Next = node2.next;
+        if (node1Prev.next == node2Prev) {
+            node1Prev.next = node2;
+            node2.next = node1;
+            node1.next = node2Next;
+        } else {
+            node1Prev.next = node2;
+            node2.next = node1.next;
+
+            node2Prev.next = node1;
+            node1.next = node2Next;
+        }
+        return dummy.next;
     }
 }
