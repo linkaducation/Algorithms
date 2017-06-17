869a6185794cd940fe4aac2e3a54a57c9529352d
iterellen@163.com <zuiaipandababy>
2017-06-17 14:05:13 +0800

    6.17

--- src/org/Algorithm/Chapter9Algorithm/GenerateTrees.java
+++ src/org/Algorithm/Chapter9Algorithm/GenerateTrees.java
@@ -11,5 +11,15 @@ import java.util.List;
 public class GenerateTrees {
     public List<TreeNode> generateTrees(int n) {
         // write your code here
+        byte b1 = 1, b2 = 3, b3, b6;
+        final byte b4 = 4, b5 = 6;
+        b6 = b4 + b5;
+        System.out.println();
+        return null;
+    }
+
+    public static void main(String[] args) {
+        GenerateTrees gt = new GenerateTrees();
+        List<TreeNode> treeNodes = gt.generateTrees(0);
     }
 }
